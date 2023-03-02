db40837bd8d36143979b727a17237e96336fa2cd
aide-abdel <49523120+aide-abdel@users.noreply.github.com>
2023-03-02 11:14:14 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+// Write an if/else statement for the following requirements:
+// - If student gets 80 or higher: console log  You did a good job
+// - If students get 70 or above: console log Keep trying
+// - If students get 60 or above: console log ehhhh
+// - If students get 55 or above: console log Not to good
+// - Any grade lower than 55 is Bad Grade But You'll Get Em Next Time
+
+grade = 90
+if(grade>=80){
+    console.log("You did a good job");
+}else if(grade>=70){
+    console.log("Keep trying");
+}else if(grade>=60){
+    console.log("ehhhh");
+}else if(grade>=55){
+    console.log("Not to good");
+}else if(grade <55){
+    console.log("Bad Grade But You'll Get Em Next Time")
+}
+
+// Write a for loop for the given output:
+//  10, 30, 50, 70, 90
+//  20, 40, 60, 80, 100
+
+for(i=10;i<100;i+=20){
+    console.log(i);
+}
+for(i=10;i<100;i+=20){
+    console.log(i+10);
+}
+
+// - This could be acheived a few ways, be creative.
+
